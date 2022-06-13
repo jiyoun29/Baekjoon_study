c39889ae4b3abdf55c3a1522853c6f84c78212c5
JIYUN <JIYUN@DESKTOP-KCHDCB7>
2022-06-14 00:00:45 +0900

    0613

---
+++ Beakjoon_study/src/Day0613/day1.java
@@ -0,0 +1,30 @@
+package Day0613;
+
+public class day1 {
+
+    public static void main(String[] args){
+        
+//        System.out.println("Hello World!");
+    	
+//        for(int i = 0; i<2; i++){
+//            System.out.println("강한친구 대한육군");
+//        }
+        
+    	
+        System.out.println("\\  /\\");
+        System.out.println(" ) ( ')");
+        System.out.println("(  /  )");
+        System.out.println("\\(__)|");  
+        
+        System.out.println("==============");
+        
+        System.out.println("|\\_/|");
+    	System.out.println("|q p|   /}");
+    	System.out.println("( 0 )\"\"\"\\");
+    	System.out.println("|\"^\"`    |");
+    	System.out.println("||_/=\\\\__|");
+        
+    }
+    
+    
+}
